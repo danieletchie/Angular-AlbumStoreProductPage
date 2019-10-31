7d8dbc27be49284a36be3067ec7c06ab6a385e9e
CM <etchiedaniel1@gmail.com>
2019-10-31 00:28:04 +0000

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,7 +21,7 @@ import { ProductListComponent } from './product-list/product-list.component';
 import { RouterModule, Routes} from '@angular/router';
 
  const appRoutes:Routes = [
-
+  {path: "products", component: ProductListComponent}
  ];
 
 @NgModule({
